2c8ccf2e91e03bc81eab2357f89e29369fa5cec8
thetechsurgeon <strengthofthemountain@gmail.com>
2024-09-28 16:07:52 -0400

    additional functionality

--- routes/search.ts
+++ routes/search.ts
@@ -22,18 +22,19 @@ module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
-      .then(([products]: any) => {
-        const dataString = JSON.stringify(products)
-        if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
-          let solved = true
-          UserModel.findAll().then(data => {
-            const users = utils.queryResultToJson(data)
-            if (users.data?.length) {
-              for (let i = 0; i < users.data.length; i++) {
-                solved = solved && utils.containsOrEscaped(dataString, users.data[i].email) && utils.contains(dataString, users.data[i].password)
-                if (!solved) {
-                  break
+    const query = buildSearchQuery(criteria);
+    executeQuery(query)
+    .then(([products]: any) => {
+      const dataString = JSON.stringify(products);
+      if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) {
+        let solved = true;
+        UserModel.findAll().then(data => {
+          const users = utils.queryResultToJson(data);
+          if (users.data?.length) {
+            for (let i = 0; i < users.data.length; i++) {
+              solved = solved && utils.containsOrEscaped(dataString, users.data[i].email) && utils.contains(dataString, users.data[i].password);
+              if (!solved) {
+                break
                 }
               }
               if (solved) {
